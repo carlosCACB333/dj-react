a3c8184b254fc55605d51a0329acdd9471397814
Carlos <carloscacb333@gmail.com>
2025-05-08 11:45:53 -0500

    feat: add PredictPriceView for price prediction based on product features

--- backend/products/urls.py
+++ backend/products/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from products.views import ProductViewSet, ProductsPDFView, SendPDFEmailView
+from products.views import (
+    ProductViewSet,
+    ProductsPDFView,
+    SendPDFEmailView,
+    PredictPriceView,
+)
 
 router = DefaultRouter()
 router.register(r"", ProductViewSet, basename="product")
@@ -8,5 +13,6 @@ router.register(r"", ProductViewSet, basename="product")
 urlpatterns = [
     path("pdf", ProductsPDFView.as_view(), name="products-pdf"),
     path("pdf/send", SendPDFEmailView.as_view(), name="send-products-pdf"),
+    path("predict-price", PredictPriceView.as_view(), name="predict-price"),
     path("", include(router.urls)),
 ]
--- backend/products/views.py
+++ backend/products/views.py
@@ -10,6 +10,10 @@ from products.models import Product
 from products.serializers import ProductSerializer
 from rest_framework.response import Response
 from rest_framework import status
+from sklearn.linear_model import LinearRegression
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.pipeline import make_pipeline
+import pandas as pd
 
 
 class ProductViewSet(viewsets.ModelViewSet):
@@ -50,6 +54,66 @@ class SendPDFEmailView(APIView):
             sender.send()
             return Response(
                 {"detail": "Email sent successfully"}, status=status.HTTP_200_OK
+            )
+        except Exception as e:
+            return Response(
+                {"detail": str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR
+            )
+
+
+class PredictPriceView(APIView):
+    permission_classes = [AllowAny]
+    allowed_currencies = ["usd", "eur", "pen"]
+
+    def post(self, request):
+        product_name = request.data.get("name")
+        product_features = request.data.get("features")
+        product_currency = request.data.get("currency", "").lower()
+
+        if not product_name or not product_features:
+            return Response(
+                {
+                    "detail": "El nombre y las características del producto son obligatorios"
+                },
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+
+        if product_currency not in self.allowed_currencies:
+            return Response(
+                {"detail": "La moneda no es válida"},
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+        if product_currency == "usd":
+            price_field = "price_usd"
+        elif product_currency == "eur":
+            price_field = "price_eur"
+        else:
+            price_field = "price_pen"
+
+        try:
+            products = Product.objects.all()
+            if products.count() < 5:
+                return Response(
+                    {
+                        "detail": "Necesitas registrar al menos 5 productos para hacer una predicción"
+                    },
+                    status=status.HTTP_400_BAD_REQUEST,
+                )
+
+            df = pd.DataFrame(list(products.values("name", "features", price_field)))
+
+            df["text"] = df["name"] + " " + df["features"]
+            X = df["text"]
+            y = df[price_field]
+
+            model = make_pipeline(TfidfVectorizer(), LinearRegression())
+            model.fit(X, y)
+
+            input_data = [product_name + " " + product_features]
+            predicted_price = model.predict(input_data)[0]
+            return Response(
+                {"predicted_price": predicted_price},
+                status=status.HTTP_200_OK,
             )
         except Exception as e:
             return Response(
